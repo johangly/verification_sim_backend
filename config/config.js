79f42d7edd6854a97f29df03dc6dfb66e2443c5d
Johangly <johanglyfabiansucre@gmail.com>
2025-08-28 03:50:18 -0400

    configurando config.js para usar el env

--- config/config.js
+++ config/config.js
@@ -3,36 +3,36 @@ env.config();
 
 const config = {
   development: {
-    username: 'root',
-    password: '221004',
-    database: 'verification_sim',
-    host: 'localhost',
-    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     dialectOptions: {
       bigNumberStrings: true,
     },
   },
-  test: {
-    username: process.env.CI_DB_USERNAME,
-    password: process.env.CI_DB_PASSWORD,
-    database: process.env.CI_DB_NAME,
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql',
-    dialectOptions: {
-      bigNumberStrings: true,
-    },
-  },
-  production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: 'mysql',
-    // dialectOptions: {
-  },
+  // test: {
+  //   username: process.env.CI_DB_USERNAME,
+  //   password: process.env.CI_DB_PASSWORD,
+  //   database: process.env.CI_DB_NAME,
+  //   host: '127.0.0.1',
+  //   port: 3306,
+  //   dialect: 'mysql',
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //   },
+  // },
+  // production: {
+  //   username: process.env.PROD_DB_USERNAME,
+  //   password: process.env.PROD_DB_PASSWORD,
+  //   database: process.env.PROD_DB_NAME,
+  //   host: process.env.PROD_DB_HOSTNAME,
+  //   port: process.env.PROD_DB_PORT,
+  //   dialect: 'mysql',
+  //   // dialectOptions: {
+  // },
 };
 
 export default config;
