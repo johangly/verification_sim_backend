2d70f3423e2a392c184860fbc81259f279ffd806
prodigy00 <calitoalejandro184@gmail.com>
2025-09-17 01:25:38 -0400

    Added migrations of phone. Create endpoint to get campaigns by id and added route

--- routes/campaigns.routes.js
+++ routes/campaigns.routes.js
@@ -26,4 +26,26 @@ router.get('/', async (req, res) => {
     res.json(campaigns);
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const campaign = await db.Campaigns.findByPk(id, {
+        include: [
+            {
+                model: db.Messages,
+                as: 'messages',
+                attributes: { exclude: ['twilioSid','phoneNumberId','createdAt'] },
+                include: [
+                    {
+                        model: db.PhoneNumbers,
+                        as: 'phoneNumber',
+                        attributes: { exclude: ['createdAt', 'updatedAt'] },
+                    },
+                ],
+            },
+        ],
+    });
+
+    res.json(campaign);
+});
+
 export default router;
