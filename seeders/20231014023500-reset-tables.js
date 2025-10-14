27b6f38027f4df6e860c4473932b602d01b64c3e
Johangly <johanglyfabiansucre@gmail.com>
2025-10-13 22:55:02 -0400

    agregando seed y comando para reinicar db

---
+++ seeders/20231014023500-reset-tables.js
@@ -0,0 +1,19 @@
+'use strict';
+
+export default {
+  up: async (queryInterface, Sequelize) => {
+    // Desactivar temporalmente las restricciones de clave foránea
+    await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', null);
+    
+    // Eliminar todos los registros de las tablas
+    await queryInterface.bulkDelete('Campaigns', null, { truncate: true, cascade: true, restartIdentity: true });
+    await queryInterface.bulkDelete('telefonos', null, { truncate: true, cascade: true, restartIdentity: true });
+    
+    // Reactivar las restricciones de clave foránea
+    await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 1', null);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // No hay acción de reversión para este seed
+  }
+};
