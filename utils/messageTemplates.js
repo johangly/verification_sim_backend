8365ed26d0394dff3ec5659edafc0b1d264e9110
Johangly <johanglyfabiansucre@gmail.com>
2025-10-17 18:37:55 -0400

    cambiando template para produccion

--- utils/messageTemplates.js
+++ utils/messageTemplates.js
@@ -1,6 +1,6 @@
 export const templates = {
     // template de si o no, verificacion
     verificationTemplate:{
-        id:"HX75e9c39ea1a499935189e07648e8ed36"
+        id:"HXc0e0ef432b8a38a7c56299025dd43a54"
     }
 }
